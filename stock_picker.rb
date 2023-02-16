8a322a988e75d914c424d770960efe5395bd9b7a
Eric Helstad <erichelstad@gmail.com>
2023-02-16 17:18:20 -0600

    Return hash of profits and days

---
+++ stock_picker.rb
@@ -0,0 +1,16 @@
+require 'pry-byebug'
+
+def stock_picker(array)
+  profits = Hash.new
+  array.each_with_index do |price, day|
+    og_day = day
+    while day < array.length
+      day_profit = array[day] - price
+      profits[day_profit] = [og_day, day]
+      day += 1
+    end
+  end
+  puts profits
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
